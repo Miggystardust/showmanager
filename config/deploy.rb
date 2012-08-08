b1234f8b78dab29e5889034f6e1bb40318ba3f8d
John Adams <jna@retina.net>
2012-08-07 21:39:29 -0700

    add asset check so that we don't compile if we don't have to

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,8 +27,18 @@ role :app, "hubba.retina.net"
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+
   task :before_update_code, :roles => :app do 
     run "whoami" 
   end 
