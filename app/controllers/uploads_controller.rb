f90d8b2cdcb167507fd999cc994cdb6b769368b0
John Adams <jna@retina.net>
2012-08-16 11:59:54 -0700

    secure the downloads method against unauthorized access or illegal filenames

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -4,10 +4,14 @@ class UploadsController < ApplicationController
   before_filter :authenticate_user!
 
   def download
-    # todo: saniize fn
-    fn = params["fn"]
-    logger.debug fn 
-    send_file "#{UPLOADS_DIR}/#{fn}", :type=>"audio/mpeg"
+      @p = Passet.where(uuid: params[:fn])[0]
+      if @p == nil
+         raise ActionController::RoutingError.new('Asset not found')
+      end
+
+      if @p.created_by == current_user.id or current_user.try(:admin?)
+        send_file "#{UPLOADS_DIR}/#{@p.uuid}", :type => @p.kind
+      end
   end
   
 end
