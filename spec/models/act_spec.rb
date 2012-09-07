d743c1e4f660dad9f7ddda5359f2ba022a5e685f
John Adams <jna@retina.net>
2012-09-07 09:42:50 -0700

    clean up acts validation and tests

--- spec/models/act_spec.rb
+++ spec/models/act_spec.rb
@@ -1,5 +1,41 @@
 require 'spec_helper'
+def valid_attributes
+  {
+    :stage_name => 'Binky',
+    :length => 600,
+    :short_description => 'Binky\'s act'
+  }
+end
 
 describe Act do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before (:each) do
+    @act = FactoryGirl.create(:act)
+  end
+
+  it "should have valid factory" do
+    FactoryGirl.build(:act).should be_valid
+  end
+
+  it "should accept whole seconds" do
+    FactoryGirl.build(:act, :length => 3600).should be_valid
+  end
+
+  it "should not accept decimal lenghts" do
+    FactoryGirl.build(:act, :length => 1.23).should_not be_valid
+  end
+
+  it "should not accept partial lenghts" do
+    FactoryGirl.build(:act, :length => "1.2").should_not be_valid
+  end
+
+  # this test can't really work - we do accept whole numbers.
+  #it "should not accept whole lenghts" do
+  #  FactoryGirl.build(:act, :length => "1").should_not be_valid
+  #end
+
+  it "should not accept empty attributes" do
+    FactoryGirl.build(:act, { :stage_name => nil, :length => nil, :short_description => nil } ).should_not be_valid
+  end
+
 end
