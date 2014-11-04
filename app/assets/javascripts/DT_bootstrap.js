811934436901e0c4b3e13c21a1c4e403416d4d5d
John Adams <jna@retina.net>
2014-11-03 17:55:56 -0800

    fix pagination for bootstrap3

--- app/assets/javascripts/DT_bootstrap.js
+++ app/assets/javascripts/DT_bootstrap.js
@@ -17,6 +17,16 @@ $.fn.dataTableExt.oApi.fnPagingInfo = function ( oSettings )
 	};
 }
 
+/* fix for bootstrap 3? unsure what this is doing... jna 11/1 */
+$.extend( true, $.fn.dataTable.defaults, {
+        "sDom": "<'row'<'col-6'f><'col-6'l>r>t<'row'<'col-6'i><'col-6'p>>",
+            "sPaginationType": "bootstrap",
+            "oLanguage": {
+            "sLengthMenu": "Show _MENU_ Rows",
+                "sSearch": ""
+                }
+    } );
+
 /* Bootstrap style pagination control */
 $.extend( $.fn.dataTableExt.oPagination, {
 	"bootstrap": {
@@ -29,12 +39,13 @@ $.extend( $.fn.dataTableExt.oPagination, {
 				}
 			};
 
-			$(nPaging).addClass('pagination').append(
-				'<ul>'+
-					'<li class="prev disabled"><a href="#">&larr; '+oLang.sPrevious+'</a></li>'+
-					'<li class="next disabled"><a href="#">'+oLang.sNext+' &rarr; </a></li>'+
-				'</ul>'
-			);
+      $(nPaging).append(
+        '<ul class="pagination">'+
+        '<li class="prev disabled"><a href="#"><i class="icon-double-angle-left"></i> '+oLang.sPrevious+'</a></li>'+
+        '<li class="next disabled"><a href="#">'+oLang.sNext+' <i class="icon-double-angle-right"></i></a></li>'+
+        '</ul>'
+      );
+
 			var els = $('a', nPaging);
 			$(els[0]).bind( 'click.DT', { action: "previous" }, fnClickHandler );
 			$(els[1]).bind( 'click.DT', { action: "next" }, fnClickHandler );
