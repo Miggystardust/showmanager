92aedaebe2efb243ee5c44668c9f998ddecdb6ef
John Adams <jna@retina.net>
2014-11-26 18:53:03 -0800

    fix date parsing and date display on bhof intros

--- config/initializers/bhof.rb
+++ config/initializers/bhof.rb
@@ -4,11 +4,11 @@ Rails3MongoidDevise::Application.configure do
   # constants, which we will move to a db at some point for the BHOF application
   ::BHOF_YEAR = '2015'
   ::BHOF_TITLE = "BHoF Weekend #{BHOF_YEAR}"
-  ::BHOF_DISCOUNT_DEADLINE =  DateTime.new(2015,1,5,0,0,00,'+8')  #'11:59pm PDT on January 19, 2015'
-  ::BHOF_FINAL_DEADLINE =  DateTime.new(2015,1,19,23,59,00,'+8')  #'11:59pm PDT on January 19, 2015'
+  ::BHOF_DISCOUNT_DEADLINE =  DateTime.new(2015,1,5,0,0,00,'-08:00')  #'11:59pm PDT on January 19, 2015'
+  ::BHOF_FINAL_DEADLINE =  DateTime.new(2015,1,19,23,59,00,'-08:00')  #'11:59pm PDT on January 19, 2015'
 
   # application rates, in cents. If the current date is after the deadline, the new rates applies
-  ::BHOF_RATES = [ { :deadline => DateTime.new(2000,1,1,0,0,00,'+8'), :rate => 2900 },
+  ::BHOF_RATES = [ { :deadline => DateTime.new(2000,1,1,0,0,00,'-08:00'), :rate => 2900 },
                    { :deadline => BHOF_DISCOUNT_DEADLINE, :rate => 3900 }]
 
 
