0b64b4121e9f144fb6555fb5a163e67b761c140c
John Adams <jna@retina.net>
2014-11-30 01:35:48 -0800

    fix redirection issue

--- app/controllers/show_items_controller.rb
+++ app/controllers/show_items_controller.rb
@@ -1,7 +1,7 @@
 class ShowItemsController < ApplicationController
   before_filter :authenticate_user!
   before_filter do
-     redirect_to :new_user_session_path unless current_user && current_user.admin?
+     redirect_to new_user_session_path unless current_user && current_user.admin?
   end
 
   # GET /show_items
--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -5,7 +5,7 @@ class ShowsController < ApplicationController
 
   before_filter :authenticate_user!
   before_filter do
-     redirect_to :new_user_session_path unless current_user && current_user.admin?
+     redirect_to new_user_session_path unless current_user && current_user.admin?
   end
 
   # GET /shows
