ded43ad036868be805e1923c05480348a8fc0758
John Adams <jna@retina.net>
2014-12-05 12:30:27 -0800

    Add BHOF to nav bars, add bhof logo, go for deploy

--- app/controllers/bhof_controller.rb
+++ app/controllers/bhof_controller.rb
@@ -1,9 +1,11 @@
 class BhofController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:index]
 
   ALLOWED_PAGES=['rules','judging_criteria','instructions','page1','page2','page3']
+
   def show
     # whitelist acceptable pages here. This controller serves primarily static pages.
+
     if ALLOWED_PAGES.include?(params[:id])
       render params[:id]
     else
@@ -12,7 +14,8 @@ class BhofController < ApplicationController
   end
 
   def index
-   redirect_to action: 'show', id: "page1"
+    # first page does not require authentication
+    render 'page1'
   end
 
 end
